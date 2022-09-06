bf56ac6b284827dba37da72ae488b1ddde7fb4d3
ЛедровЕИ <@101-08>
2022-09-06 15:52:17 +0300

    Добавьте файлы проекта.

---
+++ Examin/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Examin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Examin;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
